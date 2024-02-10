350904c702f9468eef8b4d4465e89c53a40afd6b
Goutam-04 <goutamnayak2804+github1@gmail.com>
2024-02-10 20:32:36 +0530

    fix image size in home page

--- src/components/hero/HomeCover.jsx
+++ src/components/hero/HomeCover.jsx
@@ -2,7 +2,7 @@
 // import { Facebook, Instagram, Twitter } from './Icons';
 
 // import cdpred from '../../assets/img/logo-cdpred.svg';
-// import trigger from '../../assets/img/logo-trigger.svg';
+import trigger from '../../assets/img/logo-trigger.svg';
 import xMark from '../../assets/img/x-mark.svg';
 import logoEdgerunners from '../../assets/img/logo-edgerunners.webp';
 import Image from 'next/image';
@@ -21,13 +21,13 @@ export const HomeCover = () => {
         </div>
 
         <div className="home__logos-container">
-          <a href="https://cdprojektred.com" target="_blank">
+          {/* <a href="https://cdprojektred.com" target="_blank">
             <Image src={xMark} alt="x mark" width={100} height={100} />
           </a>
           <Image src={xMark} alt="x mark" width={100} height={100} />
           <a href="https://www.st-trigger.co.jp" target="_blank">
             <Image src={xMark} alt="x mark" width={100} height={100} />
-          </a>
+          </a> */}
         </div>
 
         <div className="home__content-container">
@@ -35,8 +35,8 @@ export const HomeCover = () => {
             className="home__content-container--logo-edgerunners"
             src={logoEdgerunners}
             alt="Logo Edgerunners"
-            width={100}
-            height={100}
+            // width={100}
+            // height={100}
           />
         </div>
       </div>
