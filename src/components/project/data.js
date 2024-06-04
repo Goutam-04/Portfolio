1553df8dbd4a94eb874b28c5f2b40d51af13eb77
Goutam-04 <goutamnayak2804+github1@gmail.com>
2024-06-04 23:37:35 +0530

    project updated

--- src/components/project/data.js
+++ src/components/project/data.js
@@ -1,28 +1,28 @@
 export const projectData = [
-    {
-      id: 1,
-      title: 'Chat Site',
-      techStack: 'React, Material UI, Socket IO',
-      imageUrl: 'chat.jpg',
-      href: 'https://github.com/Goutam-04/chat-app',
-      demo: 'https://github.com/Goutam-04/chat-app',
-    },
-    {
-      id: 2,
-      title: 'Bolgging Site',
-      techStack:
-        'Next.js 13, css3, Mongo DB, OAuth, Prisma, Firebase,',
-      imageUrl: 'blog.png',
-      href: 'https://github.com/Goutam-04/Blogging',
-      demo:'https://blogging-smoky.vercel.app/'
-    },
-    {
-      id: 3,
-      title: 'Typing Game',
-      techStack:
-        'NodeJs, Express, Next.js 13,  Tailwind CSS',
-      imageUrl: 'typing.png',
-      href: 'https://github.com/Goutam-04/Typing-game',
+  {
+    id: 1,
+    title: 'Online Compiler',
+    techStack: 'Next.js, Tailwind CSS, Express.js, NodeJS, Socket.IO, Node-pty',
+    imageUrl: 'compiler.png',
+    href: 'https://github.com/Goutam-04/onlineIDE',
+    demo: 'https://online-cpp-compiler.vercel.app/',
+  },
+  {
+    id: 2,
+    title: 'Bolgging Site',
+    techStack:
+    'Next.js 13, css3, Mongo DB, OAuth, Prisma, Firebase,',
+    imageUrl: 'blog.png',
+    href: 'https://github.com/Goutam-04/Blogging',
+    demo:'https://blogging-smoky.vercel.app/'
+  },
+  {
+    id: 3,
+    title: 'Typing Game',
+    techStack:
+    'NodeJs, Express, Next.js 13,  Tailwind CSS',
+    imageUrl: 'typing.png',
+    href: 'https://github.com/Goutam-04/Typing-game',
       demo:'https://typing-game-silk.vercel.app/'
     },
     {
@@ -35,3 +35,11 @@ export const projectData = [
     },
   ];
   
+  // {
+  //   id: 1,
+  //   title: 'Chat Site',
+  //   techStack: 'React, Material UI, Socket IO',
+  //   imageUrl: 'chat.jpg',
+  //   href: 'https://github.com/Goutam-04/chat-app',
+  //   demo: 'https://github.com/Goutam-04/chat-app',
+  // },
