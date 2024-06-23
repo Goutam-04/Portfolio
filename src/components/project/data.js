978cf4bce1ac82282c36a6f4d59e30b9f9934639
Goutam-04 <goutamnayak2804+github1@gmail.com>
2024-06-23 22:45:50 +0530

    updated project data

--- src/components/project/data.js
+++ src/components/project/data.js
@@ -1,45 +1,61 @@
 export const projectData = [
   {
     id: 1,
-    title: 'Online Compiler',
-    techStack: 'Next.js, Tailwind CSS, Express.js, NodeJS, Socket.IO, Node-pty',
-    imageUrl: 'compiler.png',
-    href: 'https://github.com/Goutam-04/onlineIDE',
-    demo: 'https://online-cpp-compiler.vercel.app/',
+    title: "Online Compiler",
+    techStack: "Next.js, Tailwind CSS, Express.js, NodeJS, Socket.IO, Node-pty",
+    imageUrl: "compiler.png",
+    href: "https://github.com/Goutam-04/onlineIDE",
+    demo: "https://online-cpp-compiler.vercel.app/",
   },
   {
+    id: 6,
+    title: "Not an Ad-Blocker",
+    techStack: "JavaScript, Chrome-Extension,",
+    imageUrl: "adblocker.png",
+    href: "https://github.com/Goutam-04/NotAnAdBlocker",
+    demo: "https://not-an-ad-blocker.vercel.app/",
+  },
+  {
+    id: 7,
+    title: "Expose-port",
+    techStack: "NPM package, Javascript, Cli-tool",
+    imageUrl: "expose-port.png",
+    href: "https://github.com/Goutam-04/expose-port",
+    demo: "https://www.npmjs.com/package/expose-port",
+  },
+ 
+  {
     id: 2,
-    title: 'Blogging Site',
-    techStack:
-    'Next.js 13, css3, Mongo DB, OAuth, Prisma, Firebase,',
-    imageUrl: 'blog.png',
-    href: 'https://github.com/Goutam-04/Blogging',
-    demo:'https://blogging-smoky.vercel.app/'
+    title: "Blogging Site",
+    techStack: "Next.js 13, css3, Mongo DB, OAuth, Prisma, Firebase,",
+    imageUrl: "blog.png",
+    href: "https://github.com/Goutam-04/Blogging",
+    demo: "https://blogging-smoky.vercel.app/",
   },
   {
     id: 3,
-    title: 'Typing Game',
-    techStack:
-    'NodeJs, Express, Next.js 13,  Tailwind CSS',
-    imageUrl: 'typing.png',
-    href: 'https://github.com/Goutam-04/Typing-game',
-    },
-    {
-      id: 4,
-      title: 'E-commerce Site',
-      techStack: 'React Js, Tailwind, React-Router, Node Js, Express, MongoDB',
-      imageUrl: 'ecommerce.jpg',
-      href: 'https://github.com/Goutam-04/E-Commerce',
-      demo: 'https://github.com/Goutam-04/E-Commerce',
-    },
-   
-    {
-      id: 5,
-      title: 'Chat Site',
-      techStack: 'React, Material UI, Socket IO',
-      imageUrl: 'chat.jpg',
-      href: 'https://github.com/Goutam-04/chat-app',
-      demo: 'https://github.com/Goutam-04/chat-app',
-    },
-  ];
+    title: "Typing Game",
+    techStack: "NodeJs, Express, Next.js 13,  Tailwind CSS",
+    imageUrl: "typing.png",
+    href: "https://github.com/Goutam-04/Typing-game",
+  },
+  {
+    id: 4,
+    title: "E-commerce Site",
+    techStack: "React Js, Tailwind, React-Router, Node Js, Express, MongoDB",
+    imageUrl: "ecommerce.jpg",
+    href: "https://github.com/Goutam-04/E-Commerce",
+    demo: "https://github.com/Goutam-04/E-Commerce",
+  },
 
+  {
+    id: 5,
+    title: "Chat Site",
+    techStack: "React, Material UI, Socket IO",
+    imageUrl: "chat.jpg",
+    href: "https://github.com/Goutam-04/chat-app",
+    demo: "https://github.com/Goutam-04/chat-app",
+  },
+ 
+
+];
