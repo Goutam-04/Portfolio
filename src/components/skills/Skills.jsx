a191016c4f0f6b96aac9ffa60b406e37a4989afa
Goutam-04 <goutamnayak2804+github1@gmail.com>
2024-06-23 23:12:17 +0530

    smaller skill section

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,57 +1,53 @@
-'use client'
+"use client";
 
-import { Fade } from 'react-awesome-reveal';
-import TechCard from './TechCard';
+import { Fade } from "react-awesome-reveal";
+import TechCard from "./TechCard";
 
 const Skills = ({}) => {
   return (
-    <div id='skills' className=" col-span-2 p-6 md:col-span-2 lg:col-span-3 ">
+    <div id="skills" className=" col-span-2 p-6 md:col-span-2 lg:col-span-3 ">
       <div className="text-xl font-medium mt-2 text-primary-t mb-5 text-center">
         Skills I Have
       </div>
 
-
-      <Fade direction='down' triggerOnce>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-5 ">
-        <TechCard
-          title="Frontend"
-          classaName="bg-[#4f91f2]"
-          tech={[
-            'HTML5',
-            'CSS',
-            'Tailwind',
-            'Javascript',
-            'React Js',
-            'Next Js 13',
-            'Gatsby Js'
-          ]}
-        />
-        <TechCard
-          title="Backend"
-          classaName="bg-[#f37c36]"
-          tech={['Node Js', 'Express Js', 'Next Auth','Socket IO']}
-        />
-        <TechCard
-          title="Database"
-          classaName="bg-[#e0558a]"
-          tech={[ 'MongoDB', 'MySQL']}
-        />
-        <TechCard
-          title="Devops"
-          classaName="bg-[#10c0a2]"
-          tech={[
-            'Docker',
-            'Kubernetes',
-            'GIT',
-          ]}
-        />     <TechCard
-          title="Tools"
-          classaName="bg-[#e0558a]"
-          tech={[ 'Postman','Linux OS','Canva','Adobe XD']}
-        />
-      </div>
-      
+      <Fade direction="down" triggerOnce>
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5">
+          <TechCard
+            title="Frontend"
+            classaName="bg-[#4f91f2]"
+            tech={[
+              "HTML5",
+              "CSS",
+              "Tailwind",
+              "Javascript",
+              "React Js",
+              "Next Js 13",
+              "React-Router",
+            ]}
+          />
+          <TechCard
+            title="Backend"
+            classaName="bg-[#f37c36]"
+            tech={["Node Js", "Express Js", "Next Auth", "Socket IO", "Razorpay","NPM package"]}
+          />
+          <TechCard
+            title="Database"
+            classaName="bg-[#e0558a]"
+            tech={["MongoDB", "Mongoose", "MySQL"]}
+          />
+          <TechCard
+            title="Devops"
+            classaName="bg-[#10c0a2]"
+            tech={["Docker", "Kubernetes", "GIT"]}
+          />
+          <TechCard
+            title="Tools"
+            classaName="bg-[#e0558a]"
+            tech={["Postman", "Linux OS", "Canva", "Adobe XD"]}
+          />
+        </div>
       </Fade>
+
     </div>
   );
 };
--- src/components/skills/TechCard.jsx
+++ src/components/skills/TechCard.jsx
@@ -2,8 +2,8 @@ import { cn } from '../sidebar/Utils';
 
 const TechCard = ({ title, classaName, tech }) => {
   return (
-    <div className="flex-1 flex flex-col gap-4 bg-cardPrimary p-4 rounded-md hover:shadow-xl bg-[#132a43] ">
-      <div className="text-primary-t text-center font-medium text-lg tracking-wider">
+    <div className="flex-1 flex flex-col gap-4 bg-cardPrimary p-2 md:p-4 rounded-md hover:shadow-xl bg-[#132a43] ">
+      <div className="text-primary-t text-center font-medium text-xs lg:text-lg tracking-wider">
         {title}
       </div>
       <div className="flex flex-wrap gap-2 justify-center items-center ">
@@ -11,19 +11,22 @@ const TechCard = ({ title, classaName, tech }) => {
           <div
             key={i}
             className={cn(
-              'rounded-md bg-body text-xs text-primary-t p-2',
+              'rounded-md bg-body text-xs text-primary-t p-1.5 md:p-2',
               item === 'HTML5' && 'border border-red-500/60',
               item === 'CSS' && 'border border-blue-500/60',
               item === 'Javascript' && 'border border-amber-500/60',
               item === 'React Js' && 'border border-blue-500/60',
               item === 'Tailwind' && 'border border-sky-500/60',
               item === 'Next Js 13' && 'border border-black/60',
-              item === 'Gatsby Js' && 'border border-pink-500/60',
+              item === 'React-Router' && 'border border-pink-500/60',
               item === 'Node Js' && 'border border-green-500/60',
               item === 'Express Js' && 'border border-gray-500/60',
               item === 'Next Auth' && 'border border-purple-500/60',
               item === 'Socket IO' && 'border border-rose-500/60',
+              item === 'Razorpay' && 'border border-sky-500/60',
+              item === 'NPM package' && 'border border-red-600/60',
               item === 'MongoDB' && 'border border-emerald-500/60',
+              item === 'Mongoose' && 'border border-green-500/60',
               item === 'MySQL' && 'border border-black/60',
               item === 'Docker' && 'border border-blue-500/60',
               item === 'Kubernetes' && 'border border-blue-500/60',
